cb3e6b5a0737e69cc76124a5014a0f12ff46bff6
Aman Yadav <42231378+yadav-aman@users.noreply.github.com>
2020-09-03 12:55:25 +0530

    Create complex.h

---
+++ DS/complex.h
@@ -0,0 +1,12 @@
+typedef struct complexf{
+float real;
+float img;
+} complex;
+
+complex inv(complex a1);
+float mod(complex a1);
+complex cnj(complex a1);
+complex cadd(complex a1,complex a2);
+complex csub(complex a1,complex a2);
+complex cmult(complex a1,complex a2);
+complex cdiv(complex a1,complex a2);
